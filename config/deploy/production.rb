b3d359e3805ca1fec7834523c369bc98766384d5
Andy Milga <mail@milga.space>
2019-06-03 19:19:07 +0300

    fix deploy, fix nginx template

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,21 @@
-server "app@demo.molinos.ru", user: 'app', port: 22298, roles: %w(web app db), primary: true
-set :keep_releases, 5
-set :rails_env, 'production'
-set :branch, :master
-set :deploy_to, '/home/app/demo_adminos'
-set :whenever_path, ->{ release_path }
+server "demo.adminos.app", user: 'app', port: 22298, roles: %w(web app db), primary: true
 
+set :domain, "demo.adminos.app"
+set :keep_releases, 5
+set :rails_env, :production
+set :branch, :master
+set :user, 'app'
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :nginx_ssl_certificate, "/ssl/adminos.app-0001/fullchain.pem"
+set :nginx_ssl_certificate_key, "/ssl/adminos.app-0001/privkey.pem"
+set :nginx_ssl_dhparam, "/ssl/config/ssl-dhparams.pem"
+
+append :linked_files
+
+# CentOS
 set :nginx_sites_available_path, "#{fetch(:deploy_to)}/shared"
 set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
+
+# Ubuntu
+# set :nginx_sites_available_path, "/etc/nginx/sites-available"
+# set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,6 +8,12 @@ set :branch, :develop
 set :user, :app
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
+# ssl settings.
+# if not set and and set :nginx_use_ssl to true at deploy.rb we will try use lets encrypt certificate solution
+#set :nginx_ssl_certificate, "path to ssl certificate file"
+#set :nginx_ssl_certificate_key, "path to ssl certificate key file"
+#set :nginx_ssl_dhparam, "path to generated dhparam"
+
 # CentOS
 set :nginx_sites_available_path, "#{fetch(:deploy_to)}/shared"
 set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
