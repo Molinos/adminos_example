44720080e3d014b9ce86794a508f15e7422abadd
Эдем <keltikan@gmail.com>
2019-06-21 12:06:39 +0300

    Polls fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_131749) do
+ActiveRecord::Schema.define(version: 2019_06_21_084315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_131749) do
   end
 
   create_table "poll_options", force: :cascade do |t|
-    t.string "option"
+    t.jsonb "option"
     t.bigint "poll_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
