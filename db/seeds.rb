c1c9493bc5a784fdc1664897c1408ea55ca8971d
Эдем <keltikan@gmail.com>
2019-06-25 17:57:28 +0300

    Seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 DatabaseCleaner.clean_with(:truncation)
 
-Settings.first_or_initialize.update_attributes email: 'studio@molinos.ru', email_header_from: 'studio@molinos.ru', company_name: 'Molinos'
+Settings.first_or_initialize.update_attributes(email: 'studio@molinos.ru',
+                                               email_header_from: 'studio@molinos.ru',
+                                               company_name: 'Molinos',
+                                               twitter: 'https://twitter.com/Molinos_Ru',
+                                               facebook: 'https://www.facebook.com/molinos.ru',
+                                               github: 'https://github.com/Molinos',
+                                               copyright: 'Copyright © Molinos 2019')
+
 User.create email: 'studio@molinos.ru', password: 'changeme', roles: [:admin]
 
 # FactoryBot.create_list(:user, 10)
