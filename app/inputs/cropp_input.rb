91019c92aebcd232f7251304c2a793100db8de57
Inna Pogorelova <innaspbsu@gmail.com>
2019-06-28 13:11:25 +0300

    Bugfix/adminos 253 admin system design edits (#33)
    * edit layout and styles of admin system pages and Adminos Blog
    
    * add Back button for log page, add arrow-icon layout for sorting tables order
    
    * fix bug with deleted actiontext repo
    
    * Article view and controller updated for ordering
    
    * edit layout and styles of admin system pages and Adminos Blog
    
    * add Back button for log page, add arrow-icon layout for sorting tables order
    
    * fix bug with deleted actiontext repo
    
    * Article view and controller updated for ordering
    
    * ActionText repo actualized
    
    * Translations fixed
    
    * Exlude self from parent list
    
    * Show tags in aricles
    
    * delete unused comments

---
+++ app/inputs/cropp_input.rb
@@ -0,0 +1,67 @@
+class CroppInput < SimpleForm::Inputs::Base
+  def input(wrapper_options = nil)
+    @version = input_html_options.delete(:version) || :default
+    @coord_attribute = input_html_options.delete(:coord_attribute) || object.send("#{@version}_#{attribute_name}_attr_coord")
+
+    out = []
+    out << %{<div class="f-file">}
+    out << %{  <label class="f-file__selection js-file">}
+    out << %{    <span class="f-file__button">#{I18n.t('labels.admin.choose')}</span>}
+    out << @builder.file_field(attribute_name, input_html_options)
+    out << @builder.hidden_field(@coord_attribute)
+    out << %{    <span class="f-file__selected"></span>}
+    out << %{  </label>}
+    out << %{</div>}
+    out << preview
+
+    out.join.html_safe
+  end
+
+  def preview
+    return unless object.send(attribute_name).attached?
+    out = []
+
+    aspect_ratio = input_html_options.delete(:aspect_ratio) || '16/9'
+    aspect_ratio = aspect_ratio.split('/').map(&:to_f)
+    aspect_ratio = aspect_ratio[0] / aspect_ratio[1]
+
+    out << %{<div class="row"><div class="col-md-8"><div class="img-container">}
+
+    out << template.image_tag(object.send(attribute_name), data: { aspect_ratio: aspect_ratio, preview: ".#{@version}_#{attribute_name}_preview", toggle: 'cropp', coord: @coord_attribute })
+
+    out << %{</div></div><div class="col-md-4">}
+    out << %{<div class="cropper-dimensions">}
+    out << %{<div class="field">}
+    out << template.label_tag(:width, I18n.t('labels.admin.image.width'), class: 'control-label')
+    out << template.number_field_tag(:width, nil, class: 'form-control')
+    out << %{</div>}
+    out << %{<div class="field">}
+
+    out << template.label_tag(:height, I18n.t('labels.admin.image.height'), class: 'control-label')
+    out << template.number_field_tag(:height, nil, class: 'form-control')
+    out << %{</div>}
+
+    out << %{</div>}
+    out << %{<div class="docs-preview clearfix">}
+    out << %{<p class="figure-caption">#{I18n.t('labels.admin.image.cropped_current')}</p>}
+    out << %{<div class="img-preview preview-lg #{@version}_#{attribute_name}_preview"></div>}
+    out << cropped
+
+    out << %{</div></div></div>}
+    out.join
+  end
+
+  def cropped
+    return if @coord_attribute.blank?
+
+    out = []
+    out << %{<div class="preview-cropped"><figure class="figure">}
+
+    out << %{<figcaption class="figure-caption">#{I18n.t('labels.admin.image.cropped_saved')}</figcaption>}
+
+    out << template.image_tag(object.send("#{@version}_#{attribute_name}_cropped"))
+
+    out << %{</figure></div>}
+    out.join
+  end
+end
