6961b74b43f05e5af670d8ee6fca0a60d1f4bfc4
Zabrodin Ivan <xvanx84@gmail.com>
2020-03-19 23:55:47 +0300

    fix routes and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,33 @@
 Rails.application.routes.draw do
-    # ADMINOS ROUTES START
+  # ADMINOS ROUTES START
+  devise_for :users, skip: :omniauth_callbacks,
+    controllers: { sessions: 'users/sessions'}
 
-    devise_for :users, skip: :omniauth_callbacks,
-      controllers: { sessions: 'users/sessions'}
+  devise_for :users, skip: [:session, :password, :registration, :confirmation],
+    controllers: { omniauth_callbacks: 'authentications' }
 
-
-    devise_for :users, skip: [:session, :password, :registration, :confirmation],
-      controllers: { omniauth_callbacks: 'authentications' }
-
-    devise_scope :user do
-      get 'authentications/new', to: 'authentications#new'
-      post 'authentications/link', to: 'authentications#link'
-    end
-
+  devise_scope :user do
+    get 'authentications/new', to: 'authentications#new'
+    post 'authentications/link', to: 'authentications#link'
+  end
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
-  namespace :admin do
-    resources :features, except: :show do
-      collection { post :batch_action }
-      member { put :drop }
-      member { post :duplication }
-    end
+    namespace :admin do
+      resources :features, except: :show do
+        collection { post :batch_action }
+        member { put :drop }
+        member { post :duplication }
+      end
 
-    resources :polls, except: :show do
-      collection { post :batch_action }
-      member { post :duplication }
-    end
+      resources :polls, except: :show do
+        collection { post :batch_action }
+        member { post :duplication }
+      end
 
-    resources :articles, except: :show do
-      collection { post :batch_action }
-      member { post :duplication }
-    end
+      resources :articles, except: :show do
+        collection { post :batch_action }
+        member { post :duplication }
+      end
 
       resources :helps, only: :index
       resource  :settings, only: [:edit, :update]
@@ -59,32 +56,32 @@ Rails.application.routes.draw do
     root to: 'index#index'
   end
 
-    begin
-      Page.for_routes.group_by(&:behavior).each do |behavior, pages|
-        pages.each do |page|
-          case behavior
-          when nil
-          else
-            resource( "#{page.class.name.underscore}_#{page.id}",
-                      path:       page.absolute_path,
-                      controller: behavior,
-                      only:       :show,
-                      page_id:    page.id )
-          end
+  begin
+    Page.for_routes.group_by(&:behavior).each do |behavior, pages|
+      pages.each do |page|
+        case behavior
+        when nil
+        else
+          resource( "#{page.class.name.underscore}_#{page.id}",
+                    path:       page.absolute_path,
+                    controller: behavior,
+                    only:       :show,
+                    page_id:    page.id )
         end
       end
-    rescue
-      nil
     end
+  rescue
+    nil
+  end
 
-    # ADMINOS ROUTES END
-    devise_for :users, skip: :all
+  # ADMINOS ROUTES END
+  devise_for :users, skip: :all
 
-    root to: 'index#index'
-    resources :features, only: [:show]
-    resources :articles do
-      resources :comments
-    end
+  root to: 'index#index'
+  resources :features, only: [:show]
+  resources :articles do
+    resources :comments
+  end
 
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 DatabaseCleaner.clean_with(:truncation)
 
-Settings.first_or_initialize.update_attributes email: 'studio@molinos.ru', email_header_from: 'studio@molinos.ru', company_name: 'Molinos'
-User.create email: 'studio@molinos.ru', password: 'changeme', roles: [:admin]
+Settings.first_or_initialize.update_attributes email: 'studio@molinos.ru', email_header_from: 'studio@molinos.ru', company_name: 'Adminos'
+User.create email: 'demo@adminos.app', password: 'demoapp', roles: [:admin]
 
 # FactoryBot.create_list(:user, 10)
 # FactoryBot.create_list(:article, 30)
